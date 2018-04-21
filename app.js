02af7f3f854feefa1cd293749e1b9dbb82df6147
toeytom <toeytom2551@hotmail.com>
2018-04-21 19:29:22 +0700

    add package

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const simpleGame = require('./simpleGame')
+var numPlayer=0;
+
 app.get('/',function(req,res){
  res.sendFile(__dirname+"/game.html")
 })
 app.get('/simpleGame.js',function(req,res){
     res.sendFile(__dirname+"/simpleGame.js")
    })
+
    io.on('connection',function(socket){
-       socket.on('game',function(locationx,locationy){
-           io.emit('game',locationx,locationy)
-           console.log(locationx+" "+locationy)
+       socket.on('newplayer',function(){
+           socket.player={
+               x: randomInt(10,100),
+               y: randomInt(10,100)
+           }
+           io.emit('newplayer',socket.player)
+          
        })
-    })
+       socket.on('play',function(location){
+           io.emit('play',location)
+       })
+   })
+  
+
+   function randomInt(low,hight){
+       return Math.floor(Math.random()*(hight-low)+low)
+   }
 
 http.listen(3000,console.log("start"))
